494cec9adcb5fa7975bf0545919ac8fa29b86133
André <andre@Andres-MBP.lan>
2015-11-24 22:21:17 +0100

    cours installée design
    creation du projet

---
+++ instalike/instalike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instalike
+//
+//  Created by Andre Vandenbosch on 24/11/15.
+//  Copyright © 2015 Andre Vandenbosch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
